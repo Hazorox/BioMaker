23aaf65c5664a296f004b5b2d257232baad25b59
Hamza Mohamed Awad <hamzaawad8899@gmail.com>
2024-05-19 11:20:29 +0300

    init pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import About from './pages/about';
+
 export default function App() {
+  const [isAuth, setIsAuth] = useState(false); // Destructure the state correctly
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <div>
+        {isAuth ? <Navigate to="/profile/" /> : <Navigate to="/login" />}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile/" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }

20465697366af9e421681f4b847e8fdd624bcba3
Hamza Mohamed Awad <hamzaawad8899@gmail.com>
2024-05-28 22:51:17 +0300

    Finally fixed the grid issue in profile..

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -13,44 +13,29 @@ const Profile = () => {
   });
   return (
     <>
-      {/* <div className="h-screen flex  items-center justify-center">
-        <div className={transparent + " w-[700px] h-[500px] p-[5px] gap-[5px] grid grid-cols-3 grid-rows-3  grid-flow-row grid-areas-pic-info-info-addInfo-addInfo-addInfo-Social-Social-Social"}>
-          <div className="flex flex-col">
-          <div className="flex  w-full gap-[20px] items-center">
-            <img src={info.picture} className="w-[100px] h-[100px] rounded-full" />
-          
-          <span className="text-2xl">{info.name}</span>
-          </div>
-          <span className=" "> {info.bio} </span>
-         
-          
-          </div>
-          
-        </div>
-      </div> */}
       {/* main page */}
       <div className="h-screen flex  items-center justify-center">
         {/*transparent card*/}
+
         <div
           className={
             transparent +
-            " w-[700px] h-[500px] p-[5px] gap-[10px] grid grid-cols-3 grid-rows-3  grid-flow-row grid-areas-pic-info-info-addInfo-addInfo-addInfo-Social-Social-Social"
+            " w-[700px] h-[500px] p-[15px] gap-[10px] flex flex-col"
           }
         >
-          <div className="pic">
+          <div className="h-2/4">
             <img
               src={info.picture}
+              alt="profile picture"
               className="w-[100px] h-[100px] rounded-full"
             />
+            <div></div>
           </div>
-          {/* <div className="info h-fit overflow-hidden container grid grid-cols-3 grid-rows-3 gap-0 grid-flow-row grid-areas-Name-Name-Name-Bio-Bio-Age-Bio-Bio-Mail"> */}
-           <div className="grid grid-cols-3 grid-rows-3 gap-0 grid-flow-row">
-            <div className="Name  text-2xl">{info.name}</div>
-            <div className="Bio  ">{info.bio}</div>
-            <div className="Age ">{info.age} Years Old</div>
-            <div className="Mail">{info.email}</div>
-
+          <div className="flex h-2/5">
+            <div className="w-1/2">a1</div>
+            <div className="w-1/2">a2</div>
           </div>
+          <div className="flex h-1/6">social</div>
         </div>
       </div>
     </>

7d2f491dfa5dccc19882ff1c57d8edd0b8bd9fb5
Hamza Mohamed Awad <hamzaawad8899@gmail.com>
2024-07-11 15:00:47 +0300

    some edits

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,21 @@
-import {Schema,Model} from "mongoose";
-const userSchema = new Schema({
+// import {Schema,Model} from "mongoose";
+// const userSchema = new Schema({
+//     email:{
+//         type:String,
+//         required:true
+//     },
+//     password:{
+//         type:String,
+//         required:true
+//     },
+// })
+// const user=Model("user",userSchema)
+// export default user
+
+import mongoose from "mongoose";
+
+// Define a schema for a user document
+const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true
@@ -8,6 +24,8 @@ const userSchema = new Schema({
         type:String,
         required:true
     },
-})
-const user=Model("user",userSchema)
-export default user
+});
+
+// Create a model for the user collection based on the user schema
+const User = mongoose.model('User', userSchema);
+export default User
